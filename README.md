b0780a36849bb5f871b8facf57ba724644728ec6
jamesl1001 <jamesl1001@msn.com>
2019-05-17 02:10:26 +0100

    Push all the files

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# React-Todo-List
+# React Todo List
+
+Tutorial: https://www.youtube.com/watch?v=sBws8MSXN7A
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
---
+++ src/components/pages/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function About() {
+    return (
+        <React.Fragment>
+            <h1>About</h1>
+            <p>This is one of my first React apps.</p>
+            <p><a href="https://www.youtube.com/watch?v=sBws8MSXN7A" target="_blank" rel="noopener noreferrer">https://www.youtube.com/watch?v=sBws8MSXN7A</a></p>
+        </React.Fragment>
+    )
+}
