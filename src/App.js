cbb512b0bff3bc6498fdfc30bdbdf05e7a624ef8
jamesl1001 <jamesl1001@msn.com>
2019-05-17 02:24:15 +0100

    Fix Route links for gh-pages

--- src/App.js
+++ src/App.js
@@ -49,13 +49,13 @@ class App extends Component {
         <div className="App">
           <div className="container">
             <Header />
-            <Route exact path="/" render={props => (
+            <Route exact path="/React-Todo-List" render={props => (
               <React.Fragment>
                 <AddTodo addTodo={this.addTodo} />
                 <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
               </React.Fragment>
             )} />
-            <Route path="/about" component={About} />
+            <Route path="/React-Todo-List/about" component={About} />
           </div>
         </div>
       </Router>
