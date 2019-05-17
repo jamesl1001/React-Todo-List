a1e83e812d0b533d03b03e3dcc5c4c9ee4798d64
jamesl1001 <jamesl1001@msn.com>
2019-05-17 02:17:41 +0100

    Updates

---
+++ precache-manifest.f1778df7083a5da3950973a738081806.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2140b12224460706dadc7fcf27e5cbaa",
+    "url": "/React-Todo-List/index.html"
+  },
+  {
+    "revision": "91a4a6b2080ee2e69852",
+    "url": "/React-Todo-List/static/css/main.84e0bc13.chunk.css"
+  },
+  {
+    "revision": "94c3165186f215582bb2",
+    "url": "/React-Todo-List/static/js/2.c37d2ede.chunk.js"
+  },
+  {
+    "revision": "91a4a6b2080ee2e69852",
+    "url": "/React-Todo-List/static/js/main.cb6b63e4.chunk.js"
+  },
+  {
+    "revision": "a8a38d11c11fa70ea7a1",
+    "url": "/React-Todo-List/static/js/runtime~main.34b6e918.js"
+  }
+]);
